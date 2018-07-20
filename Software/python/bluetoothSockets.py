478ca04de23e59cdb9813d55f5575b4f7e5d1f53
kedwards999 <39536374+kedwards999@users.noreply.github.com>
2018-07-20 11:36:38 -0500

    Create bluetoothSockets.py

---
+++ Software/python/bluetoothSockets.py
@@ -0,0 +1,48 @@
+
+import bluetooth
+import time
+import common
+import parseData
+
+def CreateSocket():
+  sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+  return sock
+
+def ConnectSocket(sock, serverAddr, port):
+  sock.connect((serverAddr, port))
+  sendcount=sock.send("Hello Bluetooth Device!!!")
+
+def ReceiveData(socket):
+    receiveBuffer = ""
+    bluetoothData = ""
+    dataNotReceived = True
+
+    while (dataNotReceived):
+       receiveBuffer = socket.recv(80)
+       bluetoothData = bluetoothData + receiveBuffer
+       if (receiveBuffer.endswith("$")):
+          dataNotReceived = False
+       if (dataNotReceived):
+         x = 1
+       else:
+         parseData.parseBluetoothData(bluetoothData)
+         populateBluetoothRecord(bluetoothData)
+
+def populateBluetoothRecord(bluetoothData):
+  if (common.destination_Adapter_Id == "000" and common.source_Adapter_Id == "001"):
+    parseData.parsePirSensorData(bluetoothData)
+    common.pirSensorData = bluetoothData
+
+  if (common.destination_Adapter_Id == "000" and common.source_Adapter_Id == "004"):
+    parseData.parseGarageDoorData(bluetoothData)
+
+    common.garageDoorData = bluetoothData
+
+  if (common.destination_Adapter_Id == "000" and common.source_Adapter_Id == "005"):
+    parseData.parseCoSensorData(bluetoothData)
+    common.coSensorData = bluetoothData
+
+  if (common.destination_Adapter_Id == "000" and common.source_Adapter_Id == "006"):
+    parseData.parseGasSensorData(bluetoothData)
+    common.gasSensorData = bluetoothData
+
