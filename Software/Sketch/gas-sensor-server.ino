47037a2ae1f812cb2796d8202737c29a229d7d3b
kedwards999 <39536374+kedwards999@users.noreply.github.com>
2018-07-20 09:05:50 -0500

    Create gas-sensor-server.ino

---
+++ Software/Sketch/gas-sensor-server.ino
@@ -0,0 +1,92 @@
+#include <Chrono.h>
+#include <LightChrono.h>
+
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(3, 4);
+
+Chrono mqTime;
+int heatTime = 60000; 
+int measurementTime = 90000;
+int pwmPower = 87;
+int powerPin = 10;
+
+char dataBuffer[81]; 
+String stringBuffer;
+
+const int AOUTpin=0; 
+const int DOUTpin=8; 
+int COlimit;
+int COvalue;
+
+void setup() {
+  BTserial.begin(9600);
+  Serial.begin(9600);
+  ClearBuffer();
+  
+  while (!BTserial.available())
+  {
+    // Wait for a connected message from the HHS Controller
+  } // if(!BTserial.available())
+  BTserial.readBytes(dataBuffer,80);
+  Serial.println(dataBuffer);
+
+pinMode(DOUTpin, INPUT); 
+} // End setup()
+
+void loop() {
+
+  mqTime.restart();
+  HeatingPhase();
+  if (mqTime.hasPassed(heatTime)) 
+  {
+    mqTime.restart();
+    if (mqTime.hasPassed(measurementTime)) 
+    {
+      COlimit = digitalRead(DOUTpin);
+      COvalue = analogRead(AOUTpin); 
+      SendStatus();
+    }  
+  } // End if (mqTime.hasPassed(heatTime))
+} // End loop()
+
+void ClearBuffer()
+{
+  for (int i = 0; i < 80; i++)
+  { 
+    dataBuffer[i] = 0; 
+  } // End for (int i = 0; i < 80; i++)
+} // End ClearBuffer()
+
+void HeatingPhase()
+{
+  analogWrite(powerPin, 255);
+}
+
+void MeasurementPhase()
+{
+  analogWrite(powerPin, pwmPower);
+  analogWrite(powerPin, 255);
+}
+
+
+void SendStatus()
+{
+  // Source_Adapter_Id Destination_ Adapter_Id Max_Transmission_Count Status Data
+  // 006               000                     02                     0/1
+  if (COlimit == HIGH)
+  {
+    BTserial.print("006000021$");
+  }
+  else
+  {
+    BTserial.print("006000020$");
+  }
+  
+} // End SendStatus()
+
+
+
+
+
+
+
